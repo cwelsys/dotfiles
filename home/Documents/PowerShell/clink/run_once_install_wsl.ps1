d888e3d11994f3ecf368395a1d7da328edc2774e
Connor Welsh <cwel@cwel.sh>
2025-05-21 20:17:47 -0400

    Refactor WSL configuration and add scripts for bat and gpg management
    - Removed swap configuration from WSL config file.
    - Added a new PowerShell script to build the bat cache.
    - Introduced a PowerShell script for importing GPG keys with trust settings.
    - Added encrypted private and public GPG key files.
    - Created a PowerShell script to install WSL and manage autostart for komorebi and yasb applications.

---
+++ home/Documents/PowerShell/clink/run_once_install_wsl.ps1
@@ -0,0 +1,17 @@
+﻿if (!(Get-Command wsl -CommandType Application -ErrorAction Ignore)) {
+	Start-Process -FilePath "PowerShell" -ArgumentList "wsl", "--install" -Verb RunAs -Wait -WindowStyle Hidden
+}
+
+
+if (!(Get-Process -Name komorebi -ErrorAction SilentlyContinue)) {
+	if ($config.misc.komorebi.enable_autostart) {
+		Start-Process "powershell.exe" -ArgumentList "komorebic.exe", "enable-autostart", "--whkd" -WindowStyle Hidden -Wait
+	}
+	Start-Process "powershell.exe" -ArgumentList "komorebic.exe", "start", "--whkd" -WindowStyle Hidden
+}
+
+
+if (!(Get-Process -Name yasb -ErrorAction SilentlyContinue)) {
+	Start-Process "powershell.exe" -ArgumentList "yasbc.exe", "enable-autostart", "--task" -WindowStyle Hidden -Wait
+	Start-Process "powershell.exe" -ArgumentList "yasbc.exe", "start" -WindowStyle Hidden
+}
