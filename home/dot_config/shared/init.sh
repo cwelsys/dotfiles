09300f9533682439670028cb4e9f9eefc786b06f
Connor Welsh <cwel@cwel.sh>
2025-05-25 01:42:31 -0400

    Add encrypted configuration and scripts for private settings
    - Created `encrypted_private_periphery.config.toml.age` for secure configuration storage.
    - Added `encrypted_priv.sh.age` for encrypted private shell scripts.
    - Updated `init.sh` to source the new `priv.sh` script if it exists.
    - Enhanced `dot_zshrc` to include the installation of `atuin` for command history management.

--- home/dot_config/shared/init.sh
+++ home/dot_config/shared/init.sh
@@ -3,3 +3,4 @@
 [ -f "${HOME}/.config/shared/env.sh" ] && source "${HOME}/.config/shared/env.sh"
 [ -f "${HOME}/.config/shared/paths.sh" ] && source "${HOME}/.config/shared/paths.sh"
 [ -f "${HOME}/.config/shared/aliases.sh" ] && source "${HOME}/.config/shared/aliases.sh"
+[ -f "${HOME}/.config/shared/priv.sh" ] && source "${HOME}/.config/shared/priv.sh"
