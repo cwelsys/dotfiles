5743f4c0a10541a9e78913730ec8f229a83bd0be
Connor Welsh <cwel@cwel.sh>
2025-05-28 18:05:12 -0400

    Refactor fzf configuration and add stoyle script
    - Removed fzf configuration from env.sh.tmpl and moved it to a new stoyle.sh.tmpl file.
    - Updated init.sh to source the new stoyle.sh script.
    - Added fdz and rgz widgets to zsh for file and ripgrep searching.
    - Improved zshrc for better command handling and keybindings.
    - Adjusted completion styles and keybindings in stoyle.zsh for enhanced usability.

--- home/dot_config/shared/init.sh
+++ home/dot_config/shared/init.sh
@@ -3,4 +3,5 @@
 [ -f "${HOME}/.config/shared/env.sh" ] && source "${HOME}/.config/shared/env.sh"
 [ -f "${HOME}/.config/shared/paths.sh" ] && source "${HOME}/.config/shared/paths.sh"
 [ -f "${HOME}/.config/shared/aliases.sh" ] && source "${HOME}/.config/shared/aliases.sh"
+[ -f "${HOME}/.config/shared/stoyle.sh" ] && source "${HOME}/.config/shared/stoyle.sh"
 [ -f "${HOME}/.config/shared/priv.sh" ] && source "${HOME}/.config/shared/priv.sh"
--- home/dot_config/zsh/lib/stoyle.zsh
+++ home/dot_config/zsh/lib/stoyle.zsh
@@ -1,5 +1,35 @@
+fdz-widget() {
+	BUFFER="fdz"
+	zle accept-line
+}
+zle -N fdz-widget
+
+rgz-widget() {
+	BUFFER="rgz"
+	zle accept-line
+}
+zle -N rgz-widget
+
+bindkey '^f' fdz-widget
+bindkey '^g' rgz-widget
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+bindkey '^[w' kill-region
+bindkey ' ' magic-space
+bindkey '^U' backward-kill-line
+bindkey '^[[3;5~' kill-word
+bindkey '^[[3~' delete-char
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' backward-word
+bindkey '^[[5~' beginning-of-buffer-or-history
+bindkey '^[[6~' end-of-buffer-or-history
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+bindkey '^[[Z' undo
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' list-colors '${LS_COLORS}'
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
