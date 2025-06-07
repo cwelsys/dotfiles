32c532f60295a449e15c5e97d7f2ddb1a7b0860e
Connor Welsh <cwel@cwel.sh>
2025-06-07 14:56:24 -0400

    wt

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,8 +1,14 @@
-local Config = require("utils.class.config"):new()
+local Config = require('config')
 
-require "events.update-status"
-require "events.format-tab-title"
-require "events.new-tab-button-click"
-require "events.augment-command-palette"
+require('events.left-status').setup()
+require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
+require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'circle' })
+require('events.new-tab-button').setup()
 
-return Config:add("config"):add "mappings"
+return Config:init()
+   :append(require('config.appearance'))
+   :append(require('config.bindings'))
+   :append(require('config.domains'))
+   :append(require('config.fonts'))
+   :append(require('config.general'))
+   :append(require('config.launch')).options
