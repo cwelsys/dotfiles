b8591d96b8a4ff0f00d437be924ccda627291a41
Connor Welsh <cwel@cwel.sh>
2025-06-09 01:28:22 -0400

    🔧 Update macOS defaults: change dock autohide setting, adjust key repeat rates, and modify Finder preferences

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -5,11 +5,15 @@ set -eufo pipefail
 defaults write -g AppleShowAllExtensions -int 1
 defaults write -g NSAutomaticCapitalizationEnabled -int 0
 defaults write -g NSDocumentSaveNewDocumentsToCloud -int 0
-defaults write com.apple.dock autohide -int 1
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+defaults write com.apple.dock autohide -int 0
 defaults write com.apple.dock orientation -string left
 defaults write com.apple.dock show-recents -int 0
-defaults write com.apple.finder _FXShowPosixPathInTitle -int 1
+defaults write com.apple.finder _FXShowPosixPathInTitle -int 0
 defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
 defaults write com.apple.finder _FXSortFoldersFirst -int 1
 defaults write com.apple.finder FXRemoveOldTrashItems -int 1
 defaults write com.apple.finder FXEnableExtensionChangeWarning -int 0
+# defaults write -g com.apple.keyboard.fnState -int 1
+defaults write -g com.apple.trackpad.forceClick -int 0
