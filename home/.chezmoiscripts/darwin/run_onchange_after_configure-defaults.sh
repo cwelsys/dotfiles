ddb69233b5df41f69056404ea3f5172fe67bafa0
Connor Welsh <cwel@cwel.sh>
2025-06-10 00:06:44 -0400

    🔧 Comment out dock orientation setting in macOS defaults script

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -8,7 +8,7 @@ defaults write -g NSDocumentSaveNewDocumentsToCloud -int 0
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 defaults write com.apple.dock autohide -int 0
-defaults write com.apple.dock orientation -string left
+# defaults write com.apple.dock orientation -string left
 defaults write com.apple.dock show-recents -int 0
 defaults write com.apple.finder _FXShowPosixPathInTitle -int 0
 defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
