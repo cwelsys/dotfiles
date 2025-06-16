3ce79164ce8bfea062577369273022f65102efee
Connor Welsh <cwel@cwel.sh>
2025-06-16 16:11:21 -0400

    pwsh cleaning

---
+++ home/Documents/PowerShell/lib/psm/Reed.psm1
@@ -0,0 +1,60 @@
+﻿# Catppuccin (syntax highlighting)
+# -----------------------------------------------------------------
+# Install Catppuccin PowerShell module if not already installed
+if (-not (Get-Module -ListAvailable -Name Catppuccin -ErrorAction SilentlyContinue)) {
+	Write-Host 'Installing PowerShell Module Catppuccin...' -ForegroundColor 'Green'
+	git clone 'https://github.com/catppuccin/powershell.git' "$env:USERPROFILE\Documents\PowerShell\Modules\Catppuccin"
+}
+
+# Import required Modules
+Import-Module Catppuccin
+
+$Flavor = $Catppuccin['Mocha']
+
+#PSStyle
+$PSStyle.Formatting.TableHeader = $Flavor.Mauve.Foreground()
+$PSStyle.Formatting.CustomTableHeaderLabel = $Flavor.Mauve.Background() + $Flavor.Base.Foreground()
+$PSStyle.Formatting.Debug = $Flavor.Peach.Foreground()
+$PSStyle.Formatting.Verbose = $Flavor.Lavender.Foreground()
+$PSStyle.Formatting.FeedbackText = $Flavor.Sky.Foreground()
+$PSStyle.FileInfo.SymbolicLink = $Flavor.Blue.Foreground()
+$PSStyle.Progress.Style = $Flavor.Blue.Background() + $Flavor.Base.Foreground()
+
+# PSReadline
+# ----------------------------------------------------------------
+$PSReadLineOptions = @{
+	BellStyle                     = 'None'
+	Colors                        = @{
+		Command                = $Flavor.Blue.Foreground()
+		Comment                = $Flavor.Overlay0.Foreground()
+		ContinuationPrompt     = $Flavor.Teal.Foreground()
+		Default                = $Flavor.Text.Foreground()
+		Emphasis               = $Flavor.Lavender.Foreground()
+		Error                  = $Flavor.Red.Foreground()
+		InlinePrediction       = $Flavor.Overlay0.Foreground()
+		Keyword                = $Flavor.Mauve.Foreground()
+		ListPrediction         = $Flavor.Overlay0.Foreground()
+		ListPredictionSelected = $Flavor.Surface0.Background() + $Flavor.Mauve.Foreground()
+		Member                 = $Flavor.Rosewater.Foreground()
+		Number                 = $Flavor.Peach.Foreground()
+		Operator               = $Flavor.Yellow.Foreground()
+		Parameter              = $Flavor.Pink.Foreground()
+		Selection              = $Flavor.Surface0.Background()
+		String                 = $Flavor.Green.Foreground()
+		Type                   = $Flavor.Sky.Foreground()
+		Variable               = $Flavor.Mauve.Foreground()
+	}
+	ExtraPromptLineCount          = $True
+	HistoryNoDuplicates           = $True
+	HistorySearchCursorMovesToEnd = $True
+	MaximumHistoryCount           = 4096
+	PredictionSource              = 'HistoryAndPlugin'
+	PredictionViewStyle           = 'ListView'
+	PromptText                    = ''
+	ShowToolTips                  = $True
+}
+Set-PSReadLineOption @PSReadLineOptions
+
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
--- home/Documents/PowerShell/profile.ps1
+++ home/Documents/PowerShell/profile.ps1
@@ -13,15 +13,11 @@ $Env:LIBS = Join-Path -Path $Env:PWSH -ChildPath 'lib'
 $Env:PYTHONIOENCODING = 'utf-8'
 $env:CARAPACE_BRIDGES = 'powershell,inshellisense'
 $env:CARAPACE_NOSPACE = '*'
+$Env:_ZO_DATA_DIR = "$Env:PWSH"
 $env:SSH_AUTH_SOCK = '\\.\pipe\openssh-ssh-agent'
+$Env:GLOW_STYLE = "$HOME/.config/glow/catppuccin-mocha.json"
 
-# 🐶 FastFetch
-if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
-	if ([Environment]::GetCommandLineArgs().Contains('-NonInteractive')) {
-		Return
-	}
-	fastfetch
-}
+. "$env:LIBS\utils.ps1"
 
 # 📝 Editor
 if (Get-Command code -ErrorAction SilentlyContinue) { $Env:EDITOR = 'code' }
@@ -33,36 +29,21 @@ else {
 foreach ($module in $((Get-ChildItem -Path "$env:LIBS\psm\*" -Include *.psm1).FullName )) {
 	Import-Module "$module" -Global
 }
-foreach ($file in $((Get-ChildItem -Path "$env:LIBS\*" -Include *.ps1).FullName)) {
-	. "$file"
-}
-
-# 🐚 Prompt
-if (Get-Command starship -ErrorAction SilentlyContinue) {
-	$prompt = ''
-	function Invoke-Starship-PreCommand {
-		$current_location = $executionContext.SessionState.Path.CurrentLocation
-		if ($current_location.Provider.Name -eq 'FileSystem') {
-			$ansi_escape = [char]27
-			$provider_path = $current_location.ProviderPath -replace '\\', '/'
-			$prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
-		}
-		$host.ui.Write($prompt)
-	}
-	function Invoke-Starship-TransientFunction {
-		&starship module character
-	}
-	Invoke-Expression (&starship init powershell)
-	Enable-TransientPrompt
-}
-# elseif (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-# 	oh-my-posh init pwsh --config "$HOME\.config\posh.toml" | Invoke-Expression
+# foreach ($file in $((Get-ChildItem -Path "$env:LIBS\*" -Include *.ps1).FullName)) {
+# 	. "$file"
 # }
 
-# 💤 zoxide
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-	$Env:_ZO_DATA_DIR = "$Env:PWSH"
-	Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+# 🐚 Prompt
+function Invoke-Starship-TransientFunction {
+	&starship module character
 }
 
+Invoke-Expression (&starship init powershell)
+Enable-TransientPrompt
+Invoke-Expression (& { ( zoxide init powershell --cmd cd | Out-String ) })
 carapace _carapace | Out-String | Invoke-Expression
+
+if ([Environment]::GetCommandLineArgs().Contains('-NonInteractive')) {
+	return
+}
+fastfetch
