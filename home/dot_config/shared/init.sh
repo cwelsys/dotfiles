9a253f33531c8185cc420dc826e2cf1fca7e91a0
Connor Welsh <cwel@cwel.sh>
2025-06-28 00:48:58 -0400

    🔧 Fix command substitution syntax for `env.sh` in `init.sh`

--- home/dot_config/shared/init.sh
+++ home/dot_config/shared/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[ -f "${HOME}/.config/shared/env.sh" ] && source "${HOME}/.config/shared/env.sh"
 [ -f "${HOME}/.config/shared/paths.sh" ] && source "${HOME}/.config/shared/paths.sh"
+[ -f "${HOME}/.config/shared/env.sh" ] && source "${HOME}/.config/shared/env.sh"
 [ -f "${HOME}/.config/shared/aliae.sh" ] && source "${HOME}/.config/shared/aliae.sh"
 [ -f "${HOME}/.config/shared/stoyle.sh" ] && source "${HOME}/.config/shared/stoyle.sh"
 [ -f "${HOME}/.config/shared/priv.sh" ] && source "${HOME}/.config/shared/priv.sh"
