3303dfbb26d7b7ff6f6b4d244cf66e465693d67c
Connor Welsh <cwel@cwel.sh>
2025-10-22 16:50:41 -0400

    envs

--- home/dot_config/shell/env.sh
+++ home/dot_config/shell/env.sh
@@ -3,9 +3,86 @@
 # Source this file from your shell RC file (.zshrc, .bashrc, etc.)
 # These variables require shell features or are shell-specific
 
+# Ensure XDG Base Directories are set (fallback if /etc/profile.d didn't load)
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+export XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+export XDG_PROJECTS_DIR="${XDG_PROJECTS_DIR:-$HOME/Projects}"
+
 # Chezmoi shortcuts
-export DOTS="${XDG_DATA_HOME:-$HOME/.local/share}/chezmoi"
-export DOTFILES="${XDG_DATA_HOME:-$HOME/.local/share}/chezmoi"
+export DOTS="$XDG_DATA_HOME/chezmoi"
+export DOTFILES="$XDG_DATA_HOME/chezmoi"
+
+# Development Tools - XDG-compliant paths
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export GOPATH="$XDG_DATA_HOME/go"
+export GOBIN="$XDG_DATA_HOME/go/bin"
+export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
+export DOTNET_ROOT="$XDG_DATA_HOME/dotnet"
+export DOTNET_INSTALL_DIR="$XDG_DATA_HOME/dotnet"
+export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
+
+# Node.js / NPM
+export NPM_CONFIG_PREFIX="$XDG_DATA_HOME/npm"
+export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
+export NPM_CONFIG_INIT_MODULE="$XDG_CONFIG_HOME/npm/config/npm-init.js"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/config"
+export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_repl_history"
+export PNPM_HOME="$XDG_DATA_HOME/pnpm"
+
+# Python
+export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
+export PYTHON_HISTORY="$XDG_DATA_HOME/python/history"
+export PIPX_HOME="$XDG_DATA_HOME/pipx"
+export PIPX_GLOBAL_HOME="$XDG_DATA_HOME/pipx"
+
+# Security & Crypto
+export GNUPGHOME="$XDG_DATA_HOME/gnupg"
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME/pass"
+
+# Docker & Containers
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export VAGRANT_HOME="$XDG_DATA_HOME/vagrant"
+
+# Android
+export ANDROID_USER_HOME="$XDG_DATA_HOME/android"
+
+# NVIDIA CUDA
+export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
+
+# X11 & Display
+export XAUTHORITY="$XDG_STATE_HOME/.Xauthority"
+export XCOMPOSEFILE="$XDG_CONFIG_HOME/X11/xcompose"
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+
+# Terminal & Shell Tools
+export TERMINFO="$XDG_DATA_HOME/terminfo"
+export TERMINFO_DIRS="$XDG_DATA_HOME/terminfo:/usr/share/terminfo"
+export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
+export LESSHISTFILE="$XDG_CACHE_HOME/lesshsts"
+
+# Cloud & Infrastructure
+export AWS_CONFIG_FILE="$XDG_DATA_HOME/aws/config"
+export AWS_DATA_PATH="$XDG_DATA_HOME/aws"
+export AWS_SHARED_CREDENTIALS_FILE="$XDG_DATA_HOME/aws/credentials"
+export RCLONE_CONFIG_DIR="$XDG_CONFIG_HOME/rclone"
+
+# Misc Tools
+export BAT_CONFIG_DIR="$XDG_CONFIG_HOME/bat"
+export BAT_CONFIG_PATH="$XDG_CONFIG_HOME/bat/config"
+export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/claude"
+export GLOW_STYLE="$XDG_CONFIG_HOME/glow/catppuccin-mocha.json"
+export PARALLEL_HOME="$XDG_CONFIG_HOME/parallel"
+export WAKATIME_HOME="$XDG_CONFIG_HOME/wakatime"
+
+# Telemetry Opt-out
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DO_NOT_TRACK=1
+export DISABLE_TELEMETRY=1
 
 # Editor preferences
 export EDITOR="nvim"
