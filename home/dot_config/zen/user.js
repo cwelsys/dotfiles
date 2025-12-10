f9e51d7ed232b5195509b3a889b84f44b9a945f7
Connor Welsh <cwel@cwel.sh>
2025-12-10 11:33:43 -0500

    user.js

---
+++ home/dot_config/zen/user.js
@@ -0,0 +1,4 @@
+user_pref("dom.confirm_repost.testing.always_accept", true);
+user_pref("zen.theme.essentials-favicon-bg", false);
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
