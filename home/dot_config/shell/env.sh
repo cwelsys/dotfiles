0f44c041df620d24c37b0ac9c9e5d384108d9b21
Connor Welsh <cwel@cwel.sh>
2025-12-24 22:04:38 -0500

    boo

--- home/dot_config/shell/env.sh
+++ home/dot_config/shell/env.sh
@@ -14,7 +14,6 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 export XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}"
-export XDG_PROJECTS_DIR="${XDG_PROJECTS_DIR:-$HOME/Projects}"
 
 export DOTS="$XDG_DATA_HOME/chezmoi/home"
 export DOTFILES="$XDG_DATA_HOME/chezmoi/home"
