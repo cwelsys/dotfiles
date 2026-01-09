fc56616b0bc3e266923b008ce06d8b59e3940516
Connor Welsh <cwel@cwel.sh>
2026-01-09 00:16:10 -0500

    maps

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.clipboard = "unnamedplus"
+
 if os.getenv("SSH_TTY") then
   local osc52 = require("vim.ui.clipboard.osc52")
   vim.g.clipboard = {
