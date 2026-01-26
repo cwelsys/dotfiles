e96ef4cea789415d546082ce6f2445e1bbefb805
Connor Welsh <cwel@cwel.sh>
2026-01-26 13:45:36 -0500

    1pl
    starship hostname
    nvim osc52

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -54,13 +54,17 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufFilePost", "BufWritePost" }, {
 })
 
 vim.opt.clipboard = "unnamedplus"
-
 if os.getenv("SSH_TTY") then
-  local osc52 = require("vim.ui.clipboard.osc52")
   vim.g.clipboard = {
     name = "OSC 52",
-    copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
-    paste = { ["+"] = osc52.paste("+"), ["*"] = osc52.paste("*") },
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
   }
 end
 
