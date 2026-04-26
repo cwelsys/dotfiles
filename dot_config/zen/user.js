7e6fe5e5aebb15cce713de78e5dda21680e2a790
Connor Welsh <cwel@cwel.sh>
2026-04-25 21:03:55 -0400

    envs and js

--- dot_config/zen/user.js
+++ dot_config/zen/user.js
@@ -1,6 +1,10 @@
 user_pref("dom.confirm_repost.testing.always_accept", true);
 user_pref("zen.theme.essentials-favicon-bg", false);
+user_pref("zen.theme.accent-color", "#89b4fa");
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("widget.dmabuf.force-enabled", true);
 user_pref("gfx.font_rendering.fontconfig.max_generic_substitutions", "127");
+user_pref("security.insecure_field_warning.contextual.enabled", false);
+user_pref("security.insecure_password.ui.enabled", false);
+
